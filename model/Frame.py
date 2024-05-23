35d0c9bb054cea0e437b05cd50a61befd08a6335
wsi1212 <wsi1212@dgsw.hs.kr>
2024-05-23 14:34:07 +0900

    feat:  Create image package

--- model/Frame.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from model.Object import Object
-
-@dataclass
-class Frame:
-    frameId: int
-    objects: list(Object)
--- model/Resolution.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Resolution:
-    width: int
-    height: int
